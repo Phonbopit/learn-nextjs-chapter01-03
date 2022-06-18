cb87cfcd7408ee35515e00266c4830dd0a012fc9
Chai Phonbopit <phonbopit@gmail.com>
2022-06-19 00:44:50 +0700

    Example source code for tutorial chapter 1 - 3

---
+++ pages/about.js
@@ -0,0 +1,15 @@
+import Link from 'next/link'
+
+const AboutPage = () => {
+  return (
+    <div>
+      <h1>About</h1>
+
+      <Link href="/">
+        <a>Back home</a>
+      </Link>
+    </div>
+  )
+}
+
+export default AboutPage
